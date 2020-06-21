2e132b3dd5a9887cce833ddd70fb69e7635eb694
Shirashoji <tkm.shira@gmail.com>
2020-06-21 11:30:14 +0900

    ごめんなさい、普通に全部の場所で動いてた

--- good.js
+++ good.js
@@ -1,7 +1,9 @@
 setInterval(
     function () {
+      if (location.href == "https://twitter.com/home") {
         var likes = document.body.querySelectorAll('div[data-testid="like"]');
         for (var i = 0; i < likes.length; i++) {
             likes[i].click();
         };
+      };
     }, 1000);
